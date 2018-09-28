089522df318143a311fe173d86795b990844c6a5
jordan shartar <jordan.shartar@gmail.com>
2018-09-28 15:29:07 -0400

    browse pets
    browse pets ui. gallery option to navigate to for now. needs to be populated with dummy data. otherwise should work.

---
+++ app/src/main/java/com/example/meenal/petconnect/BrowsePets.java
@@ -0,0 +1,34 @@
+package com.example.meenal.petconnect;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrowsePets extends AppCompatActivity {
+    private ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_pets);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+////        fab.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View view) {
+////                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+////                        .setAction("Action", null).show();
+////            }
+////        });
+    }
+
+}
--- app/src/main/java/com/example/meenal/petconnect/MainActivity.java
+++ app/src/main/java/com/example/meenal/petconnect/MainActivity.java
@@ -95,7 +95,7 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
+            startActivity(new Intent(MainActivity.this, BrowsePets.class));
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
