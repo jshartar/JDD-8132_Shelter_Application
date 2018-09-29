68ca859848c7808bbfe08f4a2c49792374d292b1
Jordan Shartar <jshartar6@gatech.edu>
2018-09-29 16:33:06 -0400

    Revert "Added List view to browse pets and pet profile."
    This reverts commit 05a265cc66adfefb1d97486d189608970d31c24f.

--- app/src/main/java/com/example/meenal/petconnect/BrowsePets.java
+++ app/src/main/java/com/example/meenal/petconnect/BrowsePets.java
@@ -1,18 +1,19 @@
 package com.example.meenal.petconnect;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class BrowsePets extends AppCompatActivity {
-    private ListView listView;
-    private static CustomPetAdapter adapter;
-    private ArrayList<PetProfile> petList;
-
-
+    private ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,15 +21,14 @@ public class BrowsePets extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        listView = (ListView) findViewById(R.id.listview);
-        petList = new ArrayList<>();
-
-        petList.add(new PetProfile("Lucy", null));
-        petList.add(new PetProfile("Molly", null));
-
-        adapter = new CustomPetAdapter(petList, getApplicationContext());
-        listView.setAdapter(adapter);
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+////        fab.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View view) {
+////                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+////                        .setAction("Action", null).show();
+////            }
+////        });
     }
 
 }
-
