c738f2b4ef432d3706628802ef47cf8672fac467
Jacob <jwartofsky@gmail.com>
2018-10-17 18:57:33 -0400

    Added image selecting to RegisterPet

--- app/src/main/java/com/example/meenal/petconnect/RegisterPet.java
+++ app/src/main/java/com/example/meenal/petconnect/RegisterPet.java
@@ -1,13 +1,21 @@
 package com.example.meenal.petconnect;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class RegisterPet extends AppCompatActivity {
+
+    private static final int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +23,44 @@ public class RegisterPet extends AppCompatActivity {
         setContentView(R.layout.activity_register_pet);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ImageButton buttonLoadImage = (ImageButton) findViewById(R.id.imageButton);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageButton imageView = (ImageButton) findViewById(R.id.imageButton);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+
 
     }
 
