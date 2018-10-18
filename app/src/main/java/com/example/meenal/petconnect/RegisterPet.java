204ea6ab61efa89249598b962818e85601f00d93
Graeme Sharpe <gsharpe9@gatech.edu>
2018-10-18 17:19:07 -0400

    Added Firebase Persistance
    Admins can now register pets from the app and they will automatically be added to Firebase backend.

--- app/src/main/java/com/example/meenal/petconnect/RegisterPet.java
+++ app/src/main/java/com/example/meenal/petconnect/RegisterPet.java
@@ -9,9 +9,18 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class RegisterPet extends AppCompatActivity {
 
@@ -35,6 +44,31 @@ public class RegisterPet extends AppCompatActivity {
                         android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
                 startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+
+        Button submitButton = (Button) findViewById(R.id.RegisterPet_submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Write a message to the database
+                TextView nameView = (TextView) findViewById(R.id.RegisterPet_Name);
+                TextView ageView = (TextView) findViewById(R.id.RegisterPet_age);
+                String name = nameView.getText().toString();
+                String age = ageView.getText().toString();
+
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference ref = database.getReference("Dogs");
+                HashMap<String, Object> petDate = new HashMap<>();
+
+                petDate.put("Age", age);
+                petDate.put("Breed", age);
+                petDate.put("ChildFriendly", age);
+                petDate.put("Name", name);
+                petDate.put("NeuterSpay", age);
+                petDate.put("Pic", age);
+
+                ref.child(name).updateChildren(petDate);
             }
         });
 
