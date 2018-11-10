e593432ea35458ca0cfd6325304b14f15031af6b
Graeme Sharpe <gsharpe9@gatech.edu>
2018-11-10 15:28:56 -0500

    Update Browse Pets and Pet Profiles to be Persistance with Firebase
    Bug: Clicking on a pet will load the wrong pet profile.

--- app/src/main/java/com/example/meenal/petconnect/BrowsePets.java
+++ app/src/main/java/com/example/meenal/petconnect/BrowsePets.java
@@ -2,13 +2,29 @@ package com.example.meenal.petconnect;
 
 import android.content.Intent;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
@@ -29,11 +45,81 @@ public class BrowsePets extends AppCompatActivity {
         petList = new ArrayList<>();
         final Context context = getApplicationContext();
 
-        Drawable sparky = getResources().getDrawable(R.drawable.sparky);
-        Drawable kitty = getResources().getDrawable(R.drawable.kitty);
+//        final Drawable sparky = getResources().getDrawable(R.drawable.sparky);
+//        final Drawable kitty = getResources().getDrawable(R.drawable.kitty);
+//        final PetProfile pet = new PetProfile("Arya",kitty);
 
-        petList.add(new PetProfile("Sparky", sparky));
-        petList.add(new PetProfile("Poseidon", kitty));
+        final DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Dogs");
+        ref.orderByChild("Name").limitToFirst(25).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                final String name = dataSnapshot.getKey();
+
+                DatabaseReference tempRef = ref.child(name);
+                tempRef.child("Pic").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.getValue() != null) {
+                            final String picID = dataSnapshot.getValue().toString();
+
+                            FirebaseStorage storage = FirebaseStorage.getInstance();
+                            StorageReference storageRef = storage.getReference();
+                            StorageReference petRef = storageRef.child(picID);
+
+                            final long ONE_MEGABYTE = 1024 * 1024;
+                            petRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                                @Override
+                                public void onSuccess(byte[] bytes) {
+                                    Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                                    BitmapDrawable bitmap = new BitmapDrawable(bmp);
+
+                                    petList.add(new PetProfile(name, bitmap));
+
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception exception) {
+                                    // Handle any errors
+                                }
+                            });
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+//        Drawable sparky = getResources().getDrawable(R.drawable.sparky);
+//        Drawable kitty = getResources().getDrawable(R.drawable.kitty);
+//
+//        petList.add(new PetProfile("Sparky", sparky));
+//        petList.add(new PetProfile("Poseidon", kitty));
 
         adapter = new CustomPetAdapter(petList, getApplicationContext());
         listView.setAdapter(adapter);
--- app/src/main/java/com/example/meenal/petconnect/PetProfile_Activity.java
+++ app/src/main/java/com/example/meenal/petconnect/PetProfile_Activity.java
@@ -1,19 +1,36 @@
 package com.example.meenal.petconnect;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
 import java.util.ArrayList;
 
 public class PetProfile_Activity extends AppCompatActivity {
     private PetProfile pet;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +46,203 @@ public class PetProfile_Activity extends AppCompatActivity {
         Bitmap image = (Bitmap) getIntent().getParcelableExtra("image");
         Drawable pic = new BitmapDrawable(getResources(), image);
 
-        ImageView profile_image = (ImageView) findViewById(R.id.pet_profile_image);
-        profile_image.setImageDrawable(pic);
+        final ImageView profile_image = (ImageView) findViewById(R.id.pet_profile_image);
+//        profile_image.setImageDrawable(pic);
 
         String name = getIntent().getStringExtra("name");
-        TextView name_text = (TextView) findViewById(R.id.pet_profile_name);
+        final TextView name_text = (TextView) findViewById(R.id.pet_profile_name);
         name_text.setText(name);
-    }
 
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Dogs");
+        DatabaseReference tempRef = ref.child(name);
+        tempRef.child("Pic").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                final String picID = dataSnapshot.getValue().toString();
+
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+                StorageReference storageRef = storage.getReference();
+                StorageReference petRef = storageRef.child(picID);
+
+                final long ONE_MEGABYTE = 1024 * 1024;
+                petRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        // Data for "images/island.jpg" is returns, use this as needed
+                        Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+//                Bitmap mutableBitmap = bmp.copy(Bitmap.Config.ARGB_8888, true);
+                        BitmapDrawable bitmap = new BitmapDrawable(bmp);
+
+                        profile_image.setImageDrawable(bitmap);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        name_text.setText(exception.toString());
+                        // Handle any errors
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        // --------- Load Profile Media --------- //
+
+//        FirebaseStorage storage = FirebaseStorage.getInstance();
+//        StorageReference storageRef = storage.getReference();
+//        StorageReference petRef = storageRef.child("Arya_01.PNG");
+//
+//        final long ONE_MEGABYTE = 1024 * 1024;
+//        petRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+//            @Override
+//            public void onSuccess(byte[] bytes) {
+//                // Data for "images/island.jpg" is returns, use this as needed
+//                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+////                Bitmap mutableBitmap = bmp.copy(Bitmap.Config.ARGB_8888, true);
+//                BitmapDrawable bitmap = new BitmapDrawable(bmp);
+//
+//                profile_image.setImageDrawable(bitmap);
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception exception) {
+//                name_text.setText(exception.toString());
+//                // Handle any errors
+//            }
+//        });
+
+        // ------ Load Profile Features ------ //
+
+        final TextView aboutme_text = (TextView) findViewById(R.id.pet_profile_aboutme_deets);
+        final TextView age_text = (TextView) findViewById(R.id.pet_profile_age);
+        final TextView breed_text = (TextView) findViewById(R.id.pet_profile_breed);
+        final TextView size_text = (TextView) findViewById(R.id.pet_profile_size);
+        final TextView fixed_text = (TextView) findViewById(R.id.pet_profile_fixed);
+        final TextView fee_text = (TextView) findViewById(R.id.pet_profile_fee);
+        final TextView location_text = (TextView) findViewById(R.id.pet_profile_location);
+
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference petRef = database.child("Dogs").child(name);
+
+        petRef.child("Name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String aboutme = dataSnapshot.getValue().toString();
+                name_text.setText(aboutme);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        tempRef.child("AboutMe").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() != null) {
+                    String aboutme = dataSnapshot.getValue().toString();
+                    aboutme_text.setText(aboutme);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        tempRef.child("Age").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String aboutme = dataSnapshot.getValue().toString();
+                age_text.setText(aboutme);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        tempRef.child("Breed").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String aboutme = dataSnapshot.getValue().toString();
+                breed_text.setText(aboutme);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        tempRef.child("Size").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String aboutme = dataSnapshot.getValue().toString();
+                size_text.setText(aboutme);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        tempRef.child("NeuterSpay").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String aboutme = dataSnapshot.getValue().toString();
+                fixed_text.setText(aboutme);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        tempRef.child("Fee").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String aboutme = dataSnapshot.getValue().toString();
+                fee_text.setText(aboutme);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        tempRef.child("Location").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String aboutme = dataSnapshot.getValue().toString();
+                location_text.setText(aboutme);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        // ------ End of Data Loading ------ //
+
+    }
 }
