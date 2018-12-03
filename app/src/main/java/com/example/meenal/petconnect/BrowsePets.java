78ed47acb7cbc9d1e7c8eb28094a74c30bbc1f33
Graeme Sharpe <gsharpe9@gatech.edu>
2018-12-02 20:11:08 -0500

    Quicker Fix
    Another null fix

--- app/src/main/java/com/example/meenal/petconnect/BrowsePets.java
+++ app/src/main/java/com/example/meenal/petconnect/BrowsePets.java
@@ -135,7 +135,7 @@ public class BrowsePets extends AppCompatActivity {
                 mPets.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        final long age = (Long) ((Map) snapshot.getValue()).get("Age");
+                        final long age = (Long) ((Map) snapshot.getValue()).get("Date");
                         final String t5 = (String) ((Map) snapshot.getValue()).get("Pic");
                         if (t5 != null) {
                             FirebaseStorage storage = FirebaseStorage.getInstance();
